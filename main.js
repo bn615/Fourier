79f7b1b2962e482389f2d8f21813f56c8be619b3
bn615 <125498021+bn615@users.noreply.github.com>
2024-05-21 11:32:03 -0400

    ff

--- main.js
+++ main.js
@@ -87,6 +87,9 @@ function spacedArray(dist) {
             }
             result.push(lastPoint)
         }
+        if(isNaN(lastPoint.x) || isNaN(lastPoint.y)){
+            continue;
+        }
         if(i != points.length - 1) {
             const p1 = Complex.subtract(points[i + 1], lastPoint);
             const p = Complex.subtract(points[i], lastPoint);
@@ -95,12 +98,6 @@ function spacedArray(dist) {
             const dy = p1.y - p.y;
             const dr = Math.sqrt(dx * dx + dy * dy);
             const D = p.x * p1.y - p1.x * p.y;
-
-            if(sign(Math.pow(dist * dr, 2) - D * D) != 1){
-                lastPoint = points[i];
-                result.push(lastPoint);
-                continue;
-            }
 
             const x1 = (D * dy + sign(dy) * dx * Math.sqrt(Math.pow(dist * dr, 2) - D * D)) / (dr * dr);
             const x2 = (D * dy - sign(dy) * dx * Math.sqrt(Math.pow(dist * dr, 2) - D * D)) / (dr * dr);
