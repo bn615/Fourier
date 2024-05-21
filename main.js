96262a878b023fa6daaeae031ae5c919f9f9f482
bn615 <125498021+bn615@users.noreply.github.com>
2024-05-21 10:57:20 -0400

    f

--- main.js
+++ main.js
@@ -168,7 +168,7 @@ async function vectorAnimation() {
 
     const everyNPoints = 1;
     // let transform = fft(everyNPointsArray(everyNPoints));
-    points = spacedArray(1);
+    points = spacedArray(3);
     let transform = fft(points); 
     // 1 is the distance between points, doesnt work rn for sum reason
     let N = transform.length;
