dc1ea419062bed4ad86466cd020f995b31025933
bn615 <125498021+bn615@users.noreply.github.com>
2024-05-21 16:45:18 -0400

    dur

--- main.js
+++ main.js
@@ -190,7 +190,7 @@ async function vectorAnimation() {
 
         ctx.lineWidth = .75;
         ctx.lineCap = 'round';
-        ctx.strokeStyle = '#43a6c6';
+        ctx.strokeStyle = '#43a6cwg6';
         ctx.beginPath(); // Reset path at the beginning of each frame
 
         for (let k = 0; k < N; k++) {
@@ -205,7 +205,9 @@ async function vectorAnimation() {
 
             let currentX = currentlyAt.x;
             let currentY = currentlyAt.y;
-
+            if(k == 0){
+                continue;
+            }
             drawArrow(ctx, previousX, previousY, currentX, currentY);
         }
 
